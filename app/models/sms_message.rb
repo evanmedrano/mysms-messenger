fbf58f854f4fa9bafc9a87d8a05effa03c621c63
Evan Medrano <evan.a.medrano@gmail.com>
2025-07-23 12:04:19 -0400

    Add basic model specs

--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -1,2 +1,9 @@
 class SmsMessage < ApplicationRecord
+  belongs_to :user
+
+  # TODO: add validation for valid phone number
+
+  validates :message, presence: true, length: { maximum: 250 }
+  validates :phone_number, presence: true
+  validates :user, presence: true
 end
--- spec/models/sms_message_spec.rb
+++ spec/models/sms_message_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SmsMessage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "assoications" do
+    it { should belong_to(:user) }
+  end
+
+  describe "validations" do
+    it { should validate_length_of(:message).is_at_most(250) }
+
+    it { should validate_presence_of(:message) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_presence_of(:user) }
+  end
 end
