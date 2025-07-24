be3ad64c285302d1bc3de789aa398234b85bcf91
Evan Medrano <evan.a.medrano@gmail.com>
2025-07-24 12:22:53 -0400

    Add phone number validation

--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -1,10 +1,17 @@
 class SmsMessage < ApplicationRecord
+  PHONE_NUMBER_REGEX = /\d{3}-\d{3}-\d{4}/.freeze
+
   belongs_to :user
 
   # TODO: add validation for valid phone number
 
   validates :message, presence: true, length: { maximum: 250 }
-  validates :phone_number, presence: true
+  validates :phone_number,
+    presence: true,
+    format: {
+      with: PHONE_NUMBER_REGEX,
+      message: 'must match a XXX-XXX-XXXX format'
+    }
 
   def message_sent_at
     created_at.strftime("%A, %d-%b-%y %T %Z")
